3338dfbd60d93963b45504e04ac559955a01b933
ganaa1998 <gankhulug@gerege.mn>
2021-01-29 18:54:20 +0800

    message

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,86 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Image from 'next/image'
 
-import Player from "../components/Player";
-import PlayerCSS from "../components/PlayerCSS";
 
 export default function Home() {
-  const videoJsOptions = {
-    techOrder: ["youtube"],
-    autoplay: true,
-    controls: true,
-    sources: [
-      {
-        src: "https://www.youtube.com/watch?v=HYMDfMMD3fw&list=RDHYMDfMMD3fw&start_radio=1",
-        type: "video/youtube",
-      },
-    ],
-  };
 
+  const myLoader = ({ src, width, quality }) => {
+    return `${src}`
+  }
+  
   return (
     <div className={styles.container}>
+
+
       <Head>
-        <title>Миний NextJS сайт!</title>
+        <title>Next-js туршилт</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         Одоо л эхлэ!!!
+         Go!!!
         </h1>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Миний тухай &rarr;</h3>
-            <p>Би Next.js, React, Javascript технологиудад дуртай!</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Хобби &rarr;</h3>
-            <p>Ном унших, кино үзэх, хэл сурах, хөдөө аялах дуртай</p>
-          </a>
+          <div className={styles.card}>
+          <Image
+          loader={myLoader}
+            src="./01.jpg"
+            alt="Picture of the author"
+            width={500}
+            height={500}
+          />
+            <h3>HEELS ONLY 20$</h3>
+            <p>adidas Men's Grand Court Sneaker</p>
+          </div>
 
-          <Player {...videoJsOptions} />
-          
+
+          <div className={styles.card}>
+          <Image
+          loader={myLoader}
+            src="./02.jpg"
+            alt="Picture of the author"
+            width={500}
+            height={500}
+          />
+            <h3>$40.04 - $160.32</h3>
+            <p>New Balance Men's 608 V5 Casual Comfort Cross Trainer</p>
+          </div>
+
+
+          <div className={styles.card}>
+          <Image
+          loader={myLoader}
+            src="./03.jpg"
+            alt="Picture of the author"
+            width={500}
+            height={500}
+          />
+            <h3>$41.09 - $142.01</h3>
+            <p>BB4500 Hi 2</p>
+          </div>
+
+
+          <div className={styles.card}>
+          <Image
+          loader={myLoader}
+            src="./04.jpg"
+            alt="Picture of the author"
+            width={500}
+            height={500}
+          />
+            <h3>$56.99 - $169.26</h3>
+            <p>New Balance Men's 481 V3 Trail Running Shoe</p>
+          </div>
         </div>
+        
       </main>
+
+
+
 
       <footer className={styles.footer}>
         <a
@@ -55,6 +92,9 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
+
+
+
     </div>
   );
 }
