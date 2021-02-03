0076500eb7f3dac0bb74ec10f450e4d64d993d6b
ganaa1998 <gankhulug@gerege.mn>
2021-02-03 11:25:58 +0800

    God Question v.1.0:

--- pages/index.js
+++ pages/index.js
@@ -1,100 +1,45 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Image from 'next/image'
-
+import Image from "next/image";
+import Button from "../components/button";
 
 export default function Home() {
+	const handleClick = (e) => {
+		console.log(ChangedQuestion().props.children[0].props.children);
+		// console.log(e);
+	};
+	const AddToCart = () => (
+		<div>
+			<button onClick={handleClick} className={styles.card}>
+				<p>Refresh</p>
+			</button>
+		</div>
+	);
+	const ChangedQuestion = () => (
+		<div>
+			<h3 className={styles.text__center}>Job 30:32</h3>
+			<p>Lord teach me what i cannot see</p>
+		</div>
+	);
 
-  const myLoader = ({ src, width, quality }) => {
-    return `${src}`
-  }
-  
-  return (
-    <div className={styles.container}>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Question</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
+			<main className={styles.main}>
+				<AddToCart></AddToCart>
 
-      <Head>
-        <title>Next-js туршилт</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+				<div className={styles.grid}>
+					<div>
+						<ChangedQuestion></ChangedQuestion>
+					</div>
+				</div>
+			</main>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-         Go!!!
-        </h1>
-
-        <div className={styles.grid}>
-
-          <div className={styles.card}>
-          <Image
-          loader={myLoader}
-            src="./01.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-            <h3>HEELS ONLY 20$</h3>
-            <p>adidas Men's Grand Court Sneaker</p>
-          </div>
-
-
-          <div className={styles.card}>
-          <Image
-          loader={myLoader}
-            src="./02.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-            <h3>$40.04 - $160.32</h3>
-            <p>New Balance Men's 608 V5 Casual Comfort Cross Trainer</p>
-          </div>
-
-
-          <div className={styles.card}>
-          <Image
-          loader={myLoader}
-            src="./03.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-            <h3>$41.09 - $142.01</h3>
-            <p>BB4500 Hi 2</p>
-          </div>
-
-
-          <div className={styles.card}>
-          <Image
-          loader={myLoader}
-            src="./04.jpg"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
-            <h3>$56.99 - $169.26</h3>
-            <p>New Balance Men's 481 V3 Trail Running Shoe</p>
-          </div>
-        </div>
-        
-      </main>
-
-
-
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Технологи{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-
-
-
-    </div>
-  );
+			<footer className={styles.footer}></footer>
+		</div>
+	);
 }
