af2d017169a123acaad20a12535e8aa32c05da0b
ganaa1998 <gankhulug@gerege.mn>
2021-02-03 13:10:07 +0800

    God Question v1.1

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,33 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Image from "next/image";
-import Button from "../components/button";
+import questions from "../data/question.json";
+import Router from "next/router";
 
 export default function Home() {
-	const handleClick = (e) => {
-		console.log(ChangedQuestion().props.children[0].props.children);
-		// console.log(e);
+	const HandleClick = (e) => {
+		Router.reload(window.location.pathname);
 	};
+
 	const AddToCart = () => (
 		<div>
-			<button onClick={handleClick} className={styles.card}>
+			<button onClick={HandleClick} className={styles.card}>
 				<p>Refresh</p>
 			</button>
 		</div>
 	);
-	const ChangedQuestion = () => (
-		<div>
-			<h3 className={styles.text__center}>Job 30:32</h3>
-			<p>Lord teach me what i cannot see</p>
-		</div>
-	);
+
+	const ChangedQuestion = () => {
+		const random = Math.floor(Math.random() * questions.questions.length);
+		const retQuestionTitle = questions.questions[random].title;
+		const retQuestionContent = questions.questions[random].content;
+		console.log(retQuestionTitle, retQuestionContent);
+		return (
+			<div>
+				<div className={styles.text__center}>{retQuestionTitle}</div>
+				<p className={styles.text__center}>{retQuestionContent}</p>
+			</div>
+		);
+	};
 
 	return (
 		<div className={styles.container}>
@@ -30,13 +37,12 @@ export default function Home() {
 			</Head>
 
 			<main className={styles.main}>
-				<AddToCart></AddToCart>
-
 				<div className={styles.grid}>
 					<div>
 						<ChangedQuestion></ChangedQuestion>
 					</div>
 				</div>
+				<AddToCart></AddToCart>
 			</main>
 
 			<footer className={styles.footer}></footer>
